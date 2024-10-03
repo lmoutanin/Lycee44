ad959d7165be11e0167ea606b781d76960b4dd6a
luc.m <luc.moutanin@gmail.com>
2024-10-03 11:30:23 +0400

    tp4

---
+++ src/Controller/AbsenceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AbsenceController extends AbstractController
+{
+    #[Route('/absence', name: 'app_absence')]
+    public function index(): Response
+    {
+        return $this->render('absence/index.html.twig', [
+            'controller_name' => 'AbsenceController',
+        ]);
+    }
+}
---
+++ src/Entity/Absence.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AbsenceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AbsenceRepository::class)]
+class Absence
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $matiere = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $justifie = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getMatiere(): ?string
+    {
+        return $this->matiere;
+    }
+
+    public function setMatiere(string $matiere): static
+    {
+        $this->matiere = $matiere;
+
+        return $this;
+    }
+
+    public function getJustifie(): ?string
+    {
+        return $this->justifie;
+    }
+
+    public function setJustifie(string $justifie): static
+    {
+        $this->justifie = $justifie;
+
+        return $this;
+    }
+}
